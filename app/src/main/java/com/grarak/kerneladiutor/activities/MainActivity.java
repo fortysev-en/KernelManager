27968e7fd6cd5e1a46b7462981e464250f04961c
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2017-02-16 23:30:47 +0530

    changrs tried

--- app/src/main/java/com/grarak/kerneladiutor/activities/MainActivity.java
+++ app/src/main/java/com/grarak/kerneladiutor/activities/MainActivity.java
@@ -163,17 +163,6 @@ public class MainActivity extends BaseActivity {
         }
     }
 	
-	public class ActivityName extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // remove title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-    }
-}
 
     /**
      * Launch {@link NavigationActivity} which is the actual interface
